ebb88d800a8baedd797be07cfb03439cb9fd6635
Ryan Rodrigues <ryan.rodrigues62@hotmail.com>
2024-05-11 21:09:17 -0300

    styles: correção do skeleton

--- src/Components/Helper/Image.jsx
+++ src/Components/Helper/Image.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import styles from './Image.module.css'
 function Image({alt, ...props}) {
+    const [loading, setLoading] = React.useState(true)
+
+    function handleLoad({target}){
+        target.style.opacity = 1;
+        setLoading(false)
+    }
   return (
     <div className={styles.wrapper}>
-        <div className={styles.skeleton}></div>
-        <img className={styles.img} alt={alt} {...props} />
+        {loading && <div className={styles.skeleton}></div>}
+        <img onLoad={handleLoad} className={styles.img} alt={alt} {...props} />
     </div>
   )
 }
